f23032595bb6c90a27a6553a27be1625f3953967
Tomas_Becerra <tbecerra@sparkdigital.com>
2021-05-17 22:03:33 -0300

    no hubo ejercicio para la 5ta clase

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,5 +1,22 @@
+# #ENUNCIADO:
+# Sin tareas!! 
+
+
+
 # TEMAS:
+# diccionario = { 'key': 'value_Any_Type'}
+# diccionario['new_Key'] = 'new_Value'
+# diccionario.update(new_Element)
+# print(diccionario.values())
+# print(diccionario.keys())
+# diccionario.clear() -> it cleans the entire diccionario
+
+# POO:
+# Encapsulamiento
+# Ocultamiento: getter & setter
+# Herencia: agrupar clases similares (atributos, metodos) => jerarquía: padres, hijos
+# Abstracción: los objetos se comunican entre si.
+# Polimorfismo: sobrecarga -> no permitido en python, debido a que no se especifica el tipo de variable como parametro
+# Modularidad: subdividir la app más pequeñas. Cada clase debe hacer una cosa por si misma
+# Constructores: inizializar un objeto desde la creación
 # 
-
-
-#ENUNCIADO
